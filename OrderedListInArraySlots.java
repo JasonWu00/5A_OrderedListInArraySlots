e63d99195aaf41daa4c4b2a04ff8c53fbce55b1d
Jason Wu <zwu00@stuy.edu>
2019-03-31 17:15:27 -0400

    some work done. Trying to resolve IllegalArgumentException runtime error.

--- OrderedListInArraySlots.java
+++ OrderedListInArraySlots.java
@@ -1,3 +1,5 @@
+import java.util.Arrays; //importing Arrays so I can use a useful method
+
 public class OrderedListInArraySlots {
   private int[] elements;     // container for the elements of the list
   private int filledElements; // the number of elements in this list
@@ -57,4 +59,72 @@ public class OrderedListInArraySlots {
     elements = modified;
     return removed;
   }
+  public void add(int value){//to be used by the user
+    int position = findProperIndex(elements, value, size());
+    add(position, value);
+    filledElements++;
+  }
+
+  public void testing(int value) {
+    System.out.println(findProperIndex(elements, 3, size()));
+  }
+
+  private int findProperIndex(int[] array, int value, int currentPosition) {//helper method
+    int position = currentPosition;
+    int output = currentPosition;
+    int newCurrentPosition = 0;
+    //int[] frontHalf = Arrays.copyOfRange(array, 0, array.length/2 );
+    //int[] backHalf = Arrays.copyOfRange(array, (array.length/2 + 1), array.length);
+
+    //special cases: if array has only 0 or 1 elements
+    if (size() == 0)
+      output = 0;
+    else if (size() == 1) {
+      if (array[0] > value)
+        output = 0;
+      else
+        output = 1;
+    }
+    else {
+      int[] frontHalf = Arrays.copyOfRange(array, 0, array.length/2 );
+      int[] backHalf = Arrays.copyOfRange(array, array.length/2 +1, array.length);
+
+      if (elements[position] <= value && elements[position+1] >= value) {
+        if (elements[position] > value) {
+          output = position;
+        }
+        else {
+          output = position + 1;
+        }
+      }
+
+      else if (elements[position] < value && elements[position+1] < value){ //recursive case
+        newCurrentPosition = (int) (output * 0.5);
+        output += findProperIndex(frontHalf, value, newCurrentPosition);
+      }
+      else{
+        newCurrentPosition = (int) (output * 0.5);
+        output = findProperIndex(backHalf, value, newCurrentPosition);
+      }
+    }
+    return output;
+  }
+
+  private void add( int index, int value) { //helper function, cannot be declared by user
+    int[] modified = new int[elements.length + 1];
+    for(int elemIndex = 0; elemIndex - 1 <= filledElements ; elemIndex++) {
+      //loop to fill in new ArrayList with new value at index given
+      if (filledElements == elements.length) expand();
+      if (elemIndex < index) //when the index is not reached yet
+        modified[elemIndex] = elements[elemIndex];
+      if (elemIndex == index) { //when index is reached
+        modified[elemIndex] = value;
+      }
+      if (elemIndex > index)//after index reached
+        modified[elemIndex] = elements[elemIndex-1];
+        //shifts the index over.
+    }
+    elements = modified;
+  }
+
 }
--- UserOf_OrderedListInArraySlots.java
+++ UserOf_OrderedListInArraySlots.java
@@ -1,3 +1,19 @@
 public class UserOf_OrderedListInArraySlots {
+  private static OrderedListInArraySlots list;
 
+  public static void main(String[] commandLine) {
+    list = new OrderedListInArraySlots();
+
+    System.out.println( "number of elements: " + list.size() );
+    System.out.println( "empty list: " + list);
+    list.testing(2);
+
+    int elemIndex;
+    for( elemIndex = 0; elemIndex < 5; elemIndex++ ) {
+        list.add( elemIndex); // differs from index, but similar
+        System.out.println( "number of elements: " + list.size() );
+    }
+
+    System.out.println("initial population of " + list.size() + " elements:");
+  }
 }
